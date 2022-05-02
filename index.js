acc5826571bda7b0f962b79e8e64d07caeda82f4
davidtiemstra <79322999+davidtiemstra@users.noreply.github.com>
2022-05-02 18:59:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,23 @@ let newSymbols = [];
 let numSymbols = [[0,1,2],[3,4,5],[6,7,8],[9,10,11]];
 let allSymbols = ["📚","📓","🎓","💼","📎","📐","📄","📂","💻","🤷","❤","✎","📫","📌","📞"];
 
+const PH1 = "Hello.<br> My name is David.<br><br>Click one of the buttons on the left to learn more about me.";
+const PH2 = "Hello.<br> My name is David, and this is me up here ↑ .<br><br>Click one of the buttons on the left to learn more about me.";
+
+let checksize = 21;
+let checkers;
+let grid = [];
+let gridsize = [];
+let checkerTime = 2500;
+let checkerInterval;
+let checkerCount = 0;
+let changeCheckers;
+
 function startup(){
+
+    //DELETE THIS!!
+    document.addEventListener('keypress', keyPressed);
+
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     for (let p of urlParams) {
@@ -27,6 +43,9 @@ function startup(){
         }
         
     }
+
+    createChecker();
+
 }
 
 function toggleWindow(buttonName){
@@ -74,3 +93,88 @@ function changeSymbol(buttonTag){
     newSymbols[Math.floor(3*Math.random())] = Math.floor(allSymbols.length*Math.random())
     document.getElementById(buttonTag).innerHTML = allSymbols[newSymbols[0]] + allSymbols[newSymbols[1]] + allSymbols[newSymbols[2]];
 }
+
+function createChecker(){
+    gridsize = [Math.ceil(1.4 * window.innerWidth/checksize), Math.ceil(168/checksize)];
+    for(let i=0; i<gridsize[0]; i++){
+        // grid.push([]);
+        
+        let col = document.createElement('div');
+        col.className = "checkercolumn";
+        col.setAttribute("style", "width:" + checksize + "px;");
+        document.getElementById("checkerboard").append(col);
+
+        for(let j=0; j<gridsize[1]; j++){
+            // grid[i].push(1);
+
+            let div = document.createElement('div');
+            div.className = "checkerdiv";
+            div.id = "c" + (i*gridsize[1] + j);
+            div.setAttribute("style", "width:" + checksize + "px; height:" + checksize + "px; background-color:rgba(255, 255, 255, 1);");
+            col.append(div);
+        }
+    }
+    checkerInterval = Math.floor(checkerTime / (gridsize[0]*gridsize[1]));
+
+    checkers = false;
+    setTimeout(triggerCheckers, 8000);
+}
+
+function triggerCheckers(){
+    if(!checkers){
+        checkers = true;
+        const order = shuffledNumbers(gridsize[0]*gridsize[1]);
+        changeCheckers = setInterval(function(){changeCheck(order, 0.0)}, checkerInterval);
+        document.getElementById("placeholder").innerHTML = PH2;
+        setTimeout(triggerCheckers, 15000);
+    } else{
+        checkers = false;
+        const order = shuffledNumbers(gridsize[0]*gridsize[1]);
+        changeCheckers = setInterval(function(){changeCheck(order, 1.0)}, checkerInterval);
+        document.getElementById("placeholder").innerHTML = PH1;
+        setTimeout(triggerCheckers,45000);
+    }
+}
+
+function changeCheck(order, opac){
+    document.getElementById("c" + order[checkerCount]).style.setProperty("background-color", "rgba(255, 255, 255, "+ opac + ")");
+    checkerCount++;
+    if(checkerCount>=order.length){
+        clearInterval(changeCheckers);
+        checkerCount = 0;
+        changeCheckers = undefined;
+    }
+}
+
+function shuffledNumbers(length) {
+    let array = [];
+    for(let i=0; i<length; i++){
+        array.push(i);
+    }
+
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+  function keyPressed(e){
+    if(e.code=='KeyQ'){
+        console.log('q');
+    }
+
+    if(e.code=='KeyW'){
+        console.log('w');
+    }
+  }
