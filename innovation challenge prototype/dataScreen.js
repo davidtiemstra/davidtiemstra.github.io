6105c8c2a8a99319e2f78fbc2658ac0558676512
davidtiemstra <79322999+davidtiemstra@users.noreply.github.com>
2024-06-20 11:21:55 +0200

    uploaded innov chall

---
+++ innovation challenge prototype/dataScreen.js
@@ -0,0 +1,268 @@
+class DataScreen {
+    constructor(index, text, constants, variables, textBoxes){
+        this.index = index;
+        this.text = text;
+        this.constants = constants;
+        this.variables = variables;
+
+        // for each text thing give the coordinates and width/height
+        this.textBoxes = textBoxes;
+        
+        this.startTime = millis();
+
+        this.cloudBalls = [];
+        this.maxBall = 0.2*width;
+        this.ballDensity = 100;
+        this.ballSpeed = 0.25;
+        this.ballTime = 150;
+        this.maxBalls = 0;
+    }
+
+    // startAnimation(){
+    //     //idk best way to make this phase dependent. probably if loop.
+    //     if(this.index == 1){
+
+    //     }
+    // }
+
+    animation(){
+
+        if(this.index == 0){
+            noStroke();
+            fill("orange");
+            rect(0.2*width,0.75*height,0.6*width,0.08*height);
+            textSize(TEXTBIG);
+            textStyle(BOLD);
+            fill(0)
+            textAlign(CENTER);
+            text("DSWeetjes!", 0.5*width,0.8*height)
+            // textAlign(LEFT);
+        }
+
+        if(this.index == 1){
+            //loading screen
+            this.drawCloud(0.5*width,0.43*height,0.2*width,0.1*height, "circle");
+
+        }
+
+        if(this.index == 2){
+            // zoveel verzekerden
+            this.drawCloud(0.5*width,0.46*height,0.3*width,0.15*height, "circle");
+
+            textAlign(CENTER);
+            textSize(TEXTBIG);
+            textStyle(BOLD);
+            //this is fucked
+            fill(0, 255*((this.cloudBalls.length-this.maxBalls*0.1)/this.maxBalls*5));
+            text(min(123*round(millis()-this.startTime),this.constants.totaal), 0.37*width, 0.47*height)
+            text(this.variables.jaren, 0.53*width, 0.51*height)
+        }
+        
+        if(this.index == 3){
+            // persoonlijk 1
+            this.drawCloud(0.5*width,0.46*height,0.3*width,0.15*height, "circle");
+
+            textAlign(CENTER);
+            textSize(TEXTBIG);
+            textStyle(BOLD);
+            //this is fucked
+            fill(0, 255*((this.cloudBalls.length-this.maxBalls*0.1)/this.maxBalls*5));
+            text(this.variables[0], 0.48*width, 0.4*height);
+
+        }
+        
+        if(this.index == 4){
+            // verdeling zorgkosten
+            this.drawCloud(0.1*width,0.06*height,0.8*width,0.2*height, "rect");
+
+            textAlign(CENTER);
+            textSize(TEXTBIG);
+            textStyle(BOLD);
+            //this is fucked
+            fill(0, 255*((this.cloudBalls.length-this.maxBalls*0.1)/this.maxBalls*5));
+            text(this.constants.totaal, 0.72*width, 0.11*height)
+
+            let i = 0;
+            let total=0;
+            for(const cost in this.constants){
+                if(this.constants[cost] == this.constants.totaal){
+                    continue;
+                }
+
+                const val = min(0.1*(millis()-this.startTime), this.constants[cost])
+                
+                const startAngle = total/103 * 2*PI;
+                total+=val;
+                const endAngle = total/103 * 2*PI;
+                total+=0.5;
+
+                const colOffset = noise(i*100)*100-20;
+                fill(13+colOffset,105+colOffset,55+colOffset);
+                stroke(0);
+                strokeWeight(2);
+                let pieSize = 0.4;
+                let pos=createVector(0.5,0.65);
+
+                beginShape();
+                vertex(
+                    pos.x*width + cos((endAngle-startAngle)*0.5+startAngle)*width*0.01,
+                    pos.y*height+ sin((endAngle-startAngle)*0.5+startAngle)*width*0.01
+                );
+                const res = val*2;
+                for(let j=0;j<res;j++){
+                    vertex(
+                        pos.x*width + cos((endAngle-startAngle)*j/res+startAngle)*width*pieSize*(0.9+0.1*noise(i,0.15*j,0.001*millis())),
+                        pos.y*height+ sin((endAngle-startAngle)*j/res+startAngle)*width*pieSize*(0.9+0.1*noise(i,0.15*j,0.001*millis())));
+                }
+                vertex(
+                    pos.x*width + cos(endAngle)*width*pieSize,
+                    pos.y*height+ sin(endAngle)*width*pieSize
+                );
+                
+                endShape(CLOSE);
+
+                noStroke();
+                fill(0);
+                
+                textAlign(CENTER);
+                textSize(TEXTBIG);
+                textStyle(BOLD);
+                let textPos=createVector(
+                    pos.x*width + cos((endAngle-startAngle)*(0.4+i/6)+startAngle)*width*pieSize*(0.3+i/8),
+                    pos.y*height+ sin((endAngle-startAngle)*(0.4+i/6)+startAngle)*width*pieSize*(0.3+i/8));
+                text(this.constants[cost] + "%",
+                    textPos.x,
+                    textPos.y - 12
+                );
+
+                textSize(TEXTSMLL);
+                textStyle(NORMAL);
+                textAlign(CENTER);
+                text(cost,
+                    textPos.x,
+                    textPos.y);
+            
+                i++;
+            }
+
+        }
+        
+        if(this.index == 5){
+            // persoonlijk 2
+            this.drawCloud(0.5*width,0.5*height,0.3*width,0.15*height, "circle");
+
+            textAlign(CENTER);
+            textSize(TEXTBIG);
+            textStyle(BOLD);
+            //this is fucked
+            fill(0, 255*((this.cloudBalls.length-this.maxBalls*0.1)/this.maxBalls*5));
+            text(this.variables[0], 0.48*width, 0.4*height);
+            
+        }
+        
+        if(this.index == 6){
+            // zoveelin de buurt
+            this.drawCloud(0.5*width,0.46*height,0.3*width,0.15*height, "circle");
+
+            textAlign(CENTER);
+            textSize(TEXTBIG);
+            textStyle(BOLD);
+            //this is fucked
+            fill(0, 255*((this.cloudBalls.length-this.maxBalls*0.1)/this.maxBalls*5));
+            text(this.variables[0], 0.48*width, 0.4*height);
+            
+        }
+        
+        if(this.index == 7){
+            // gerda
+            this.drawCloud(0.5*width,0.46*height,0.3*width,0.15*height, "circle");
+            
+        }
+        
+        if(this.index == 8){
+            // jaarwens
+            this.drawCloud(0.5*width,0.46*height,0.3*width,0.15*height, "circle");
+            
+        }
+        
+        if(this.index == 9){
+            // delen
+            this.drawCloud(0.1*width,0.06*height,0.8*width,0.8*height, "rect");
+
+        }
+    }
+
+    displayText(){
+        textSize(TEXTSTD);
+        textStyle(NORMAL);
+        textAlign(CENTER);
+        fill(0, 255*((this.cloudBalls.length-this.maxBalls*0.1)/this.maxBalls*5));
+
+        for(let i=0; i< this.text.length;i++){
+            text(
+                this.text[i], 
+                this.textBoxes[i][0] * width, 
+                this.textBoxes[i][1] * height, 
+                this.textBoxes[i][2] * width, 
+                this.textBoxes[i][3] * height
+            )
+        }
+    }
+
+    drawCloud(x,y,width,height,shape){
+
+        if(shape=="rect"){
+            this.maxBalls = this.ballDensity * (width*height) / (PI*this.maxBall*this.maxBall);
+        }
+        if(shape=="circle"){
+            const surface = ((PI*width*width) + (PI*height*height))/2
+            this.maxBalls = this.ballDensity * (surface) / (PI*this.maxBall*this.maxBall);
+        }
+
+        // if(this.cloudBalls.length<this.maxBalls && random()>this.ballSpeed){
+        if(random()>(this.cloudBalls.length+0.01)/this.maxBalls){
+            if(shape=="rect"){
+               this.cloudBalls.push({
+                    x:x+random()*width,
+                    y:y+random()*height,
+                    size:0,
+                    time:0
+                }); 
+            }
+            if(shape=="circle"){
+                let angle = random()*360;
+                let dist = random();
+                this.cloudBalls.push({
+                    x:x+cos(angle)*dist*width,
+                    y:y+sin(angle)*dist*height,
+                    size:0,
+                    time:0
+                }); 
+            }
+            
+        }
+
+        fill(0);
+        for(let i=this.cloudBalls.length-1; i>=0; i--){
+            if(this.cloudBalls[i].time++ < this.ballTime){
+                this.cloudBalls[i].size += (this.maxBall - this.cloudBalls[i].size) * 0.05;
+            }
+            else{
+                this.cloudBalls[i].size += (-1*this.maxBall - this.cloudBalls[i].size) * 0.025;
+                if(this.cloudBalls[i].size<0){
+                    this.cloudBalls.splice(i,1);
+                    continue;
+                }
+            }
+            
+            circle(this.cloudBalls[i].x,this.cloudBalls[i].y,this.cloudBalls[i].size+4);
+        }
+        fill(252)
+        for(let i=this.cloudBalls.length-1; i>=0; i--){
+            circle(this.cloudBalls[i].x,this.cloudBalls[i].y,this.cloudBalls[i].size);
+        }
+
+
+    }
+
+}
---
+++ innovation challenge prototype/sketch.js
@@ -0,0 +1,310 @@
+let phase = -1;
+
+let screens = [];
+
+let naam;
+let woonplaats;
+let jaren;
+let hoevaakGebeld;
+let hoevaakLogin;
+let snelsteDecla;
+
+let movePhase = false;
+
+const TEXTBIG = 24;
+const TEXTSTD = 18;
+const TEXTSMLL = 14;
+
+const ALWAYSTEXT = [
+    [],
+    ["Jouw reis aan het bouwen..."],
+    [
+        "We zijn met", 
+        "verzekerden!", 
+        "en jij bent al", 
+        "jaar deel van de familie"
+    ],
+    ["Dat is hoevaak jij ingelogd hebt op de app! Jij blijft graag op de hoogte!"],
+    [
+        "We hebben met zijn allen voor", 
+        "euro aan zorgkosten vergoed!", 
+        "Dit is waar het naartoe ging:"
+    ],
+    ["Je kan ons altijd bellen! Dit is hoevaak jij met de klantenservice van DSW gepraat hebt! Hopelijk wordt volgend jaar net zo gezellig!"],
+    [
+        "Je bent niet alleen!", 
+        "Van jouw buren is", 
+        "lid bij DSW!"],
+    [
+        "Jij bent een...", 
+        "persona idk"],
+    ["We wensen je een gezond 2024!"],
+    [
+        "Dit is het overzicht van jouw DSW reis:", 
+        "Delen?"]
+];
+
+//define as fractions of the full width/height
+const ALWAYSTEXTBOXES = [
+    [], //button
+    [[0.35, 0.4, 0.3, 0.1]], //loading text
+    [
+        [0.35, 0.41, 0.3, 0.2],
+        [0.45, 0.45, 0.25, 0.2],
+        [0.25, 0.49, 0.3, 0.1],
+        [0.34, 0.53, 0.4, 0.1]
+    ], // hoeveel verzekerden 
+    [[0.23, 0.45, 0.5, 0.4]], // personal: standard center box
+    [
+        [0.11, 0.09, 0.5, 0.2],
+        [0.41, 0.13, 0.5, 0.1],
+        [0.2, 0.20, 0.4, 0.3],
+    ], // zorgkosten waarheen
+    [[0.23, 0.45, 0.5, 0.4]], // personal: standard center box
+    [
+        [0.15, 0.05, 0.8, 0.3],
+        [0.12, 0.82, 0.5, 0.2],
+        [0.39, 0.89, 0.8, 0.1]
+    ], // wie in de buurt
+    [
+        [0.15, 0.05, 0.8, 0.3],
+        [0.12, 0.82, 0.8, 0.2],
+    ], // rad van fortuin
+    [[0.25, 0.45, 0.5, 0.4]], //jaarwens: center box
+    [
+        [0.15, 0.05, 0.8, 0.3],
+        [0.5,0.8,0.4,0.2]
+    ] //status
+]
+
+const VARIABLEBOXES = [
+    [],
+    [],
+]
+
+let alwaysData = [
+    null, //app homepage
+    null, //loading
+    {
+        totaal: 570000,
+    }, //aantal verzekerden
+    null, //persoonlijk
+    {
+        totaal: "1,7 miljard",
+        ziekenhuis: 67.8,
+        farmacie: 13.8,
+        huisarts: 10.3,
+        hulpmiddelen: 4.9,
+        fysio: 3.1
+    }, //verdeling zorgkosten (totaal-> per ding)
+    null, //persoonlijk
+    {
+        schiedam: 60,
+    }, //link naar heatmap plaatje met verzekerden verdeling i guess????
+    ["gerda", "mark", "viktor"], //de namen waar we uit kiezen
+    null, //jaarwens
+    null, //profiel
+];
+
+let alwaysVariables = [
+    null,
+    null,
+    0, //jaar lid
+    null,
+    null,
+    null,
+    ["woonplaats"], //woonplaats
+    null, //weet ik nog niet
+    null,
+    null, //weet ik nog niet
+];
+
+const SOMETIMESTEXT = [
+    [],
+];
+
+let sometimesVariables = [
+
+];
+
+let backgrounds = [];
+let bg;
+let bgar;
+
+function preload() {
+    for(let i=0; i<ALWAYSTEXT.length; i++){
+        backgrounds.push(loadImage("backgrounds/"+i+".jpg"))
+    }
+    bg = loadImage("backgrounds/tekening1.png");
+}
+
+function setup() {
+    createCanvas(windowWidth - 0, windowHeight - 4);
+
+    bgar = bg.width/bg.height;
+    //prepare setup for personal data
+    randomButton = createButton("Vul willekeurige waarden in").mousePressed(randomizeVariables);
+    randomButton.position(20,40);
+    
+    naamInput = createInput("verzekerde");
+    naamInput.position(20, 100);
+
+    woonplaatsInput = createInput();
+    woonplaatsInput.position(20, 160);
+
+    jarenInput = createSlider(1, 40, round(random()*39+1));
+    jarenInput.position(20, 220);
+
+    hoevaakGebeldInput = createSlider(0, 40, round(random()*40));
+    hoevaakGebeldInput.position(20, 280);
+
+    hoevaakLoginInput = createSlider(0, 200, round(random()*200));
+    hoevaakLoginInput.position(20, 340);
+
+    snelsteDeclaInput = createSlider(0, 60, round(random()*60));
+    snelsteDeclaInput.position(20, 400);
+
+    submitButton = createButton("Start prototype!").mousePressed(initializeScreens);
+    submitButton.position(20, 440);
+
+    //calculate variables for always shown screens
+
+
+    // 
+}
+
+function draw() {
+    background(255);
+
+    if(movePhase){
+        if(screens[phase].cloudBalls.length <1){
+            phase++
+            movePhase = false;
+            screens[phase].startTime = millis();
+        }
+    }
+
+    // during setup everything is gonna be DOM so dont show shit
+    if(phase == -1){
+        text("Naam: ", 
+            20, 95);
+        text("Woonplaats: ", 
+            20, 155);
+        text("Aantal jaar verzekerd bij DSW: " + jarenInput.value() + " jaar", 
+            20, 215);
+        text("Aantal keer gebeld met DSW: " + hoevaakGebeldInput.value() + " keer", 
+            20, 275);
+        text("Aantal keer ingelogd bij de app: " + hoevaakLoginInput.value() + " keer", 
+            20, 335);
+        text("Snelste declaratie verwerkingstijd: " + snelsteDeclaInput.value() + " minuten", 
+            20, 395);
+        return;
+    }
+
+    // display screens!
+    if(phase == 0 ){
+        image(backgrounds[phase],0,0,width,height)
+    }
+    else{
+        background("#83D0F0");
+        image(bg,millis()*-0.0001,0,height*bgar,height)
+    }
+
+    screens[phase].animation();
+    screens[phase].displayText();
+
+    textAlign(LEFT)
+    // text(floor(frameRate()),10,10)
+
+}
+
+function mouseClicked(){
+    print(mouseX/width, mouseY/height)
+    
+    if(phase>=0){
+        if(!movePhase){
+            screens[phase].ballDensity = 0.0001;
+            movePhase=true;
+        }else{
+            phase++
+            movePhase = false;
+            screens[phase].startTime = millis();
+        }
+    }
+}
+
+function keyPressed(){
+    if(keyCode===ENTER){
+        if(!movePhase){
+            screens[phase].ballDensity = 0.0001;
+            movePhase=true;
+        }else{
+            phase++
+            movePhase = false;
+            screens[phase].startTime = millis();
+        }
+    }
+}
+
+function randomizeVariables(){
+    naamInput.value(alwaysData[7][floor(random()*alwaysData[7].length)]);
+    woonplaatsInput.value("schiedam");
+    jarenInput.value(round(random()*39+1));
+    hoevaakGebeldInput.value(round(random()*40));
+    hoevaakLoginInput.value( round(random()*200));
+    snelsteDeclaInput.value( round(random()*60));
+
+}
+
+function initializeScreens(){
+    // set values and delete all the setup bullshit
+    naam = naamInput.value();
+    woonplaats = woonplaatsInput.value();
+    jaren = jarenInput.value();
+    hoevaakGebeld = hoevaakGebeldInput.value();
+    hoevaakLogin = hoevaakLoginInput.value();
+    snelsteDecla = snelsteDeclaInput.value();
+
+    randomButton.remove()
+    naamInput.remove();
+    woonplaatsInput.remove();
+    jarenInput.remove();
+    hoevaakGebeldInput.remove();
+    hoevaakLoginInput.remove();
+    snelsteDeclaInput.remove();
+    submitButton.remove();
+
+
+    // calculate which screens to show & put in the data (index, text, textVariables, textBoxes)
+    
+    alwaysVariables[2] = {
+        totaal: alwaysData[2].totaal,
+        jaren: jaren
+    }
+    alwaysVariables[3] = [
+        //persoonlijk 1
+        hoevaakLogin
+    ]
+    alwaysVariables[5] = [
+        //persoonlijk 2
+        hoevaakGebeld
+    ]
+    alwaysVariables[6] = {
+        //locatie
+    }
+    
+    for(let i=0; i< ALWAYSTEXT.length; i++){
+        screens.push(new DataScreen(
+            i,
+            ALWAYSTEXT[i],
+            alwaysData[i],
+            alwaysVariables[i],
+            ALWAYSTEXTBOXES[i]
+        ));
+    }
+
+    console.log(screens);
+
+    // start!!!
+    phase = 0;
+}
